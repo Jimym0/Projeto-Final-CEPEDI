531cab6aaa5ad33640632751dea197f6bc4cd309
Jimym0 <jimym004@gmail.com>
2026-03-11 16:37:39 -0300

    style: correções gramaticais

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,6 @@
-# accounts/models.py
 from django.contrib.auth.models import User
 from django.db import models
 
-# Se precisar de perfil extra:
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True, null=True)
